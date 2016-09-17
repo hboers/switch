c81d9c9fb78836c157aad8d7e7857ef5a87b9ab9
Heinrich Boers <mail@heinrich-boers.net>
2016-09-17 18:06:47 +0200

    config

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Security-Policy" content="connect-src *;" />
+<meta http-equiv="Content-Security-Policy" content="default-src 'self' 192.168.178.*;" />
  
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
